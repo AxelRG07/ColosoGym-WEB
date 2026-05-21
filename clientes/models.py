8069013ebdf290798347ee6165d0ac3104bbde3c
AxelRG07 <axelrepeat@gmail.com>
2026-05-21 01:29:02 -0600

    Creación del proyecto. App clientes implementada en conjunto con modelos, serializers, viewset y urls. API de clientes funcionando.

---
+++ clientes/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Cliente(models.Model):
+    ESTADOS_CHOICES = [
+        ('Activo', 'Activo'),
+        ('Inactivo', 'Inactivo'),
+    ]
+
+    codigo_barra = models.CharField(max_length=50, unique=True, blank=True)
+    nombre_completo = models.CharField(max_length=150)
+    telefono_emergencia = models.CharField(max_length=20)
+    notas_medicas = models.TextField(null=True, blank=True)
+    estado = models.CharField(max_length=15, choices=ESTADOS_CHOICES, default='Activo')
+    fecha_registro = models.DateField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.codigo_barra:
+            ultimo_cliente = Cliente.objects.order_by('-id').first()
+
+            if ultimo_cliente:
+                siguiente_numero = ultimo_cliente.id + 1
+            else:
+                siguiente_numero = 1
+
+            self.codigo_barra = f"CG-{str(siguiente_numero).zfill(3)}"
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nombre_completo
